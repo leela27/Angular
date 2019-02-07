6f0c1f67ff5fc024679945714b0f7256fd4bc14f
Leela27 <aleelan1@in.ibm.com>
2019-02-07 10:26:57 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { FormsModule } from '@angular/forms';
+
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TrackModule } from './track/track.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    TrackModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
